67f725bbe1cf99af22a23a13cb6756c814f6139e
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-02-03 21:13:51 -0800

    feat: add dropdown list with api

--- pages/add.js
+++ pages/add.js
@@ -1,4 +1,38 @@
-export default () => <div>
-    <h1>Add to my Pokedex</h1>
-    Test
-</div>;
+import React from 'react'
+import fetch from 'isomorphic-unfetch'
+import Select from 'react-select'
+
+export default class extends React.Component {
+    static async getInitialProps({ req }) {
+        let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=964");
+        response = await response.json();
+        let pokemonNames = response.results;
+        // let pokemonPromises = [];
+        // for (let pokemon of list) {
+        //     pokemonPromises.push(fetch(pokemon.url).then(e => e.json()));
+        // }
+        // list = await Promise.all(pokemonPromises);
+        // console.log(list[0]);
+        return { pokemonList: pokemonNames.map(e => ({label: e.name, value: e.url}) ) }
+    }
+
+    render() {
+        return (
+            <div>
+                <Select
+                    closeMenuOnSelect={false}
+                    defaultValue={[]}
+                    isMulti
+                    options={this.props.pokemonList}
+                />
+                Hello World
+        {/* <table>{this.props.pokemonList.map(pokemon =>
+                    <tr key={pokemon.name}>
+                        <td>{pokemon.name}</td>
+                        <td><img src={pokemon.sprites.front_default} /></td>
+                    </tr>)}
+                </table> */}
+            </div>
+        )
+    }
+}
